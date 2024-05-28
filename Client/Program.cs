70c6733113bfff2d0381212c0c09daa0e2b4425f
rakuzzan <marishka1581@gmail.com>
2024-05-28 03:10:48 +0300

    lw6

---
+++ Client/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client;
+
+class Program
+{
+    public static void StartClient(string hostaddress, int port, string msg)
+    {
+        try
+        {
+            // Разрешение сетевых имён
+            IPAddress ipAddress = (hostaddress == "localhost") ? IPAddress.Loopback : IPAddress.Parse(hostaddress);
+
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+            // CREATE
+            Socket sender = new Socket(
+                ipAddress.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            try
+            {
+                // CONNECT
+                sender.Connect(remoteEP);
+
+                // SEND
+                int bytesSent = sender.Send(Encoding.UTF8.GetBytes(msg));
+
+                // RECEIVE
+                byte[] buf = new byte[1024];
+                int bytesRec = sender.Receive(buf);
+
+                Console.WriteLine("{0}",
+                    Encoding.UTF8.GetString(buf, 0, bytesRec));
+
+                // RELEASE
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        if (args.Length != 3)
+        {
+            Console.WriteLine("Invalid usage.\nUsage: dotnet run <hostAddress> <port> <msg>");
+            return;
+        }
+        string hostAddress = args[0];
+        bool success = int.TryParse(args[1], out int port);
+        string msg = args[2];
+
+        if (String.IsNullOrEmpty(msg))
+        {
+            Console.WriteLine("Empty message.");
+            return;
+        }
+
+        if (!success)
+        {
+            Console.WriteLine("Invalid port");
+            return;
+        }
+
+        StartClient(hostAddress, port, msg);
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Server;
+
+class Program
+{
+    public static void StartListening(int port)
+    {
+        // Привязываем сокет ко всем интерфейсам на текущей машинe
+        IPAddress ipAddress = IPAddress.Any;
+
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+
+        // CREATE
+        Socket listener = new Socket(
+            ipAddress.AddressFamily,
+            SocketType.Stream,
+            ProtocolType.Tcp);
+
+        try
+        {
+            // BIND
+            listener.Bind(localEndPoint);
+
+            // LISTEN
+            listener.Listen(10);
+
+            List<string> history = new List<string>();
+
+            while (true)
+            {
+                // ACCEPT
+                Socket handler = listener.Accept();
+
+                byte[] buf = new byte[1024];
+                string data = null;
+                // RECEIVE
+                int bytesRec = handler.Receive(buf);
+
+                data = Encoding.UTF8.GetString(buf, 0, bytesRec);
+
+                Console.WriteLine("Message received: {0}", data);
+                
+                history.Add(data);
+
+                // Создаем объект StringBuilder для объединения всех строк
+                StringBuilder stringBuilder = new StringBuilder();
+
+                // Объединяем все строки в одну с разделителем (например, новая строка)
+                foreach (string str in history)
+                {
+                    stringBuilder.AppendLine(str);
+                }
+
+                // Преобразуем объединенную строку в массив байт с помощью UTF-8 кодировки
+                byte[] msg = Encoding.UTF8.GetBytes(stringBuilder.ToString());
+
+                // SEND
+                handler.Send(msg);
+
+                // RELEASE
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Invalid usage\nUsage: dotnet run <port>");
+            return;
+        }
+        bool success = int.TryParse(args[0], out int port);
+        if (!success)
+        {
+            Console.WriteLine("Invalid port");
+            return;
+        }
+        StartListening(port);
+    }
+}
