3fbb93ac9ec1c39112b7ee32312563d7a375609f
rakuzzan <marishka1581@gmail.com>
2024-06-12 13:36:49 +0300

    lw3 fix

--- RankCalculator/obj/Release/net8.0/RankCalculator.AssemblyInfo.cs
+++ RankCalculator/obj/Release/net8.0/RankCalculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RankCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f6e1bc14cd6700a2a39869df79ab3bb331102ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b237997324d3fbbfec64422e8ea151191f05afd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("RankCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RankCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Valuator/Pages/Index.cshtml.cs
+++ Valuator/Pages/Index.cshtml.cs
@@ -81,3 +81,4 @@ public class IndexModel : PageModel
 
         return false;
     }
+}
